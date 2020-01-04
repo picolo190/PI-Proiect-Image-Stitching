e5a174627a113967aed9ca9e2e8bd9bce1c07ae6
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-01-04 14:03:04 +0200

    Ask user for a save file name.

--- PI_Proiect/main.cpp
+++ PI_Proiect/main.cpp
@@ -2,16 +2,15 @@
 
 int main(void)
 {
-	string image_name_1("Images/uni_1.jpeg");
-	string image_name_2("Images/uni_2.jpeg");
-	Mat image_1 = imread(image_name_1);
-	Mat image_2 = imread(image_name_2);
+	//Read the images to be stitched
+	string image_name_1("Images/uni_1.jpeg"), image_name_2("Images/uni_2.jpeg");
+	Mat image_1 = imread(image_name_1), image_2 = imread(image_name_2);
 
 	checkImage(image_1);
 	checkImage(image_2);
 
+	//Create the gray images of the ones read before
 	Mat gray_image_1, gray_image_2;
-
 	cvtColor(image_1, gray_image_1, COLOR_BGR2GRAY);
 	cvtColor(image_2, gray_image_2, COLOR_BGR2GRAY);
 
@@ -24,13 +23,30 @@ int main(void)
 	//Calculate the homography
 	Mat homography = calculateHomography(image1_desc_kp, image2_desc_kp);
 
-	//Stitch images
+	//Stitch the images
 	Mat final_image = stitchImage(image_1, image_2, homography);
 
 	//Display the result
 	imshow("window", final_image);
 
-	string path("./Images/result2.png");
+	//Save the image as a file
+	bool ok = false;
+	string path("Images/");
+
+	while (!ok) 
+	{
+		string aux;
+		cout << "\nEnter the name of the file to contain the new image:";
+		cin >> aux;
+		if (aux != "")
+		{
+			path += aux;
+			path += ".png";
+			ok = true;
+		}
+	}
+
+
 	saveImage(final_image, path);
 
 	waitKey(0);
